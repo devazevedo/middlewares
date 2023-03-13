57b7f515542b20a70d79e4ef0d4897fd2e6aeecb
azevedoafc <azevedoafc23@gmail.com>
2023-03-13 19:54:14 -0300

    completando os middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,68 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(404).json({error: "Username not found"});
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  const isPro = user.todo === true;
+
+  const userTodos = user.todos.lenght <= 10;
+
+  if(!isPro && !userTodos){
+    return response.status(403).json({error: "Seu plano não permite mais todos."});
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  const todo = user.find(todo => todo.id === id);
+
+  if(!user){
+    return response.status(404).json({error: "Username not exists"});
+  }
+
+  if(!todo){
+    return response.status(400).json({error: "Todo not exists"});
+  }
+
+  request.user = user;
+  request.todo = todo; 
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if(!user){
+    return response.status(404).json({error: "User not exists"})
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
